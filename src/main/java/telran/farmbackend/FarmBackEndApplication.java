3f205751dc78c711e747f105b2ec768fee8da7a1
Alexander <algurev0220@gmail.com>
2025-06-15 13:24:51 +0300

    Farm back-end (beginning)

---
+++ src/main/java/telran/farmbackend/FarmBackEndApplication.java
@@ -0,0 +1,13 @@
+package telran.farmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/telran/farmbackend/FarmBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.farmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
